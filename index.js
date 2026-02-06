51cf83f37b5f4c6b42477b1872b598eb5d88cc95
naafnadim5-crypto <naafnadim5@gmail.com>
2026-02-06 10:21:16 -0500

    fixed: upadated port and host for railway

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
- const express = require('express');
+const express = require('express');
 const app = express();
+
+// রেলওয়ে এই PORT ভেরিয়েবলটা ব্যবহার করে, তাই এটি অত্যন্ত জরুরি
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! My first Node app is live on Railway.');
+  res.send('Hello World! My first Node app is officially live on Railway.');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
