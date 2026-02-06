4d20c7f8376f1b64fbd6e5ec1883ffd7b2bb7137
naafnadim5-crypto <naafnadim5@gmail.com>
2026-02-06 09:45:38 -0500

    update port for railway

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
